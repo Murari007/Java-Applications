7701959445cd1c5525a341ed91064b86c7816aa0
Murari007 <01fe20bec084@kletech.ac.in>
2023-05-10 14:48:50 +0530

    Deleted Inheritance
    Deleted

--- Inheritance/Dog.java
+++
@@ -1,8 +0,0 @@
-package org.meicode.Inheritance;
-
-public class Dog extends Animal
-{
-    public Dog(String name, String color, int legs, boolean hasTail) {
-        super(name, color, legs, hasTail);
-    }
-}
